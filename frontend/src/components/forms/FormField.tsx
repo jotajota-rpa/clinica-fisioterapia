a8b980f26b436992ef53e969d06cdef02c63b082
Juan Jose Rodriguez-Prat <jjrodriguezprat@gmail.com>
2026-03-24 23:20:42 +0100

    feat: Hecha la UI de Ajustes-Clinica

--- frontend/src/components/forms/FormField.tsx
+++ frontend/src/components/forms/FormField.tsx
@@ -8,32 +8,23 @@ interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export const FormField = ({ label, error, helperText, ...props }: FormFieldProps) => {
   return (
-    <div className="flex flex-col gap-1.5 w-full max-w-sm mb-4">
+    <div className="w-full max-w-xs p-2 shadow-md">
       {label && (
-        <label className="text-sm font-semibold text-gray-700 ml-0.5">
-            {label}
-        </label>
+        <label className="block mb-2"> {label} </label>
       )}
 
       <input {...props}
-        className={`px-3 py-2 text-sm border rounded-md outline-none transition-all duration-200 placeholder:text-gray-400
-          ${error
-            ? 'border-red-500 focus:ring-4 focus:ring-red-100'
-            : 'border-gray-300 focus:border-green-500 focus:ring-4 focus:ring-green-50/50 shadow-sm'
-          }
+        className={`w-full border rounded-lg p-2 focus:ring-2 transition-all duration-300 ease-in-out outline-none
+            ${error ? 'border-red-400 focus:ring-red-500' : 'border-gray-400 focus:ring-green-500'}
         `}
       />
 
       {error && (
-        <span className="text-[11px] text-red-500 font-medium ml-0.5">
-          {error}
-        </span>
+        <span className="text-red-500"> {error} </span>
       )}
 
-      {helperText && !error && (
-        <span className="text-[11px] text-gray-400 ml-0.5">
-          {helperText}
-        </span>
+      {helperText && !error &&(
+        <span className="text-gray-500"> {helperText} </span>
       )}
     </div>
   );
--- frontend/src/pages/ajustes/AjustesClinica.tsx
+++ frontend/src/pages/ajustes/AjustesClinica.tsx
@@ -5,19 +5,26 @@ import type { AjustesClinicaDTO } from '../../interfaces/InterfacesApis';
 import { FormField } from '../../components/forms/FormField';
 
 function AjustesClinica() {
+
     return (
-        <form className="">
-            <h1>Configuración</h1>
+        <div className="flex h-screen w-full items-center justify-center bg-gray-100 min-w-[300px]">
+        <form className="flex flex-col items-center justify-center bg-white rounded-2xl shadow-xl shadow-black/50 w-full max-w-md p-10">
+            <h1 className="font-bold p-4 m-2 text-xl text-center">Configuración Ajustes Clínica</h1>
 
-            <FormField label="Caaa / NIF" placeholder="Introduce el CIF..." />
+            <FormField label="CIF / NIF" placeholder="Introduce el CIF..." />
             <FormField label="Nombre Clínica" placeholder="Ej: Fisioterapia Relax" />
+            <FormField label="Dirección" placeholder="Ej: Calle Júpiter" />
+            <FormField label="Teléfono" placeholder="900112233" />
+            <FormField label="Correo Eléctronico" placeholder="clinica@email.com" />
+            <FormField label="Serie de Facturación" placeholder="FXX" />
 
 
-            <button type="submit">
+            <button className="bg-green-500 text-white font-bold m-2 py-3 px-5 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:bg-gray-900 hover:shadow-black/40 active:scale-95 active:shadow-inner">
               Guardar Ajustes
             </button>
         </form>
+        </div>
     )
- }
+}
 
 export default AjustesClinica
